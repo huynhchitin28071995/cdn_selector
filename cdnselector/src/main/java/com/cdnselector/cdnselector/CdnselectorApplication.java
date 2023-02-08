a2e3d8eabd956ebc3e7fd4b4ab88e7d9415a3db8
Huynh Chi Tin <>
2023-02-08 21:01:57 +0700

    init project with dockerfile, docker-compose, spring-boot configuration

---
+++ cdnselector/src/main/java/com/cdnselector/cdnselector/CdnselectorApplication.java
@@ -0,0 +1,13 @@
+package com.cdnselector.cdnselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnselectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdnselectorApplication.class, args);
+	}
+
+}
